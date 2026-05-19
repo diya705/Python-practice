90d506b04b05f36ead44654ed126c4516c870eae
Diya Agarwal <diyaagarwal705@gmail.com>
2026-05-19 16:54:58 +0530

    Daily update 19-05-2026

--- string.py
+++ string.py
@@ -1,4 +1,6 @@
-# use replace method into string take is taken from user 
+# use replace method into string take is taken from user
+
+
 a = input("Enter a string: ")
 print(a.replace(a, "yes")) # input is using to take the input from the user and store it in a variable
 
@@ -48,4 +50,61 @@ def count_substring(string, sub_string):
         if string[i:].startswith(sub_string):
             count += 1
     return count
-            
+
+
+# str.isalnum()
+# This method checks if all the characters of a string are alphanumeric (a-z, A-Z and 0-9).
+
+print('ab123'.isalnum())
+# True
+print('ab123#'.isalnum())
+# False
+str.isalpha()
+# This method checks if all the characters of a string are alphabetical (a-z and A-Z).
+
+print('abcD'.isalpha())
+# True
+print('abcd1'.isalpha())
+# False
+str.isdigit()
+# This method checks if all the characters of a string are digits (0-9).
+
+print('1234'.isdigit())
+# True
+print('123edsd'.isdigit())
+# False
+str.islower()
+# This method checks if all the characters of a string are lowercase characters (a-z).
+
+print('abcd123#'.islower())
+# True
+print('Abcd123#'.islower())
+# False
+str.isupper()
+# This method checks if all the characters of a string are uppercase characters (A-Z).
+
+print('ABCD123#'.isupper())
+# True
+print('Abcd123#'.isupper())
+# False
+
+
+# use string slicing with the syntax text[0:5].
+# In Python, the stop index is exclusive, so 0:5 will extract indexes 0, 1, 2, 3, and 4.
+# Example Code
+# python
+text = "HackerRank"
+
+# One-line code to extract and print index 0 to 4
+print(text[0:5])  # Output: Hacke
+
+# using the imported textwrap module:
+#     Since the challenge already imports textwrap for you, you can also solve it using the built-in module directly in one line:
+#         python
+import textwrap
+def wrap(string, max_width):
+    return textwrap.fill(string, max_width)
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
